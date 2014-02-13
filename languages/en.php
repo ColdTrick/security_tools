13ef332b5f412f22125d2b65cec8f6d217cb2664
Jeroen Dalsem <jdalsem@coldtrick.com>
2014-02-13 11:13:48 +0100

    added new settings

--- languages/en.php
+++ languages/en.php
@@ -5,11 +5,14 @@
 
 $english = array(
 	'security_tools' => "Security Tools",
-	
+
 	// settings
 	'security_tools:settings:secure_upgrade' => "Secure upgrade.php",
 	'security_tools:settings:secure_upgrade:description' => "With a secured upgrade.php only logged in site administrators can run upgrade.php",
-	
+
+	'security_tools:settings:disable_autocomplete_on_password_fields' => "Disable autocomplete on password fields",
+	'security_tools:settings:disable_autocomplete_on_password_fields:description' => "Data entered in these fields will be cached by the browser. An attacker who can access the victim's browser could steal this information. This is especially important if the application is commonly used in shared computers such as cyber cafes or airport terminals. If you disable this, password management tools can no longer autofill these fields.",
+
 );
 
 add_translation("en", $english);
