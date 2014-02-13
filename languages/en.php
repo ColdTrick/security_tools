76d7ff65bc19374477b5a179677fe91f49e7c1da
Jerome Bakker <jeabakker@coldtrick.com>
2014-02-13 13:38:27 +0100

    notify administrator of new administrators and removal of administrators

--- languages/en.php
+++ languages/en.php
@@ -5,16 +5,33 @@
 
 $english = array(
 	'security_tools' => "Security Tools",
-
+	
 	// settings
 	'security_tools:settings:secure_upgrade' => "Secure upgrade.php",
 	'security_tools:settings:secure_upgrade:description' => "With a secured upgrade.php logged in site administrators can run upgrade.php or you'll need a special code (see link below).",
-
+	
 	'security_tools:settings:disable_autocomplete_on_password_fields' => "Disable autocomplete on password fields",
 	'security_tools:settings:disable_autocomplete_on_password_fields:description' => "Data entered in these fields will be cached by the browser. An attacker who can access the victim's browser could steal this information. This is especially important if the application is commonly used in shared computers such as cyber cafes or airport terminals. If you disable this, password management tools can no longer autofill these fields.",
-
+	
 	// site secret
-	'security_tools:site_secret:warning' => "After you regenerate the site secret, <a href='%s'>please check out</a> the new Upgrade.php code you'll need."
+	'security_tools:site_secret:warning' => "After you regenerate the site secret, <a href='%s'>please check out</a> the new Upgrade.php code you'll need.",
+	
+	// admin notifications
+	'security_tools:notify_admins:make_admin:subject' => "A new site administrator was assigned for %s",
+	'security_tools:notify_admins:make_admin:message' => "Hi,
+
+The user %s was assigned as a new site administrator by %s.
+Check out the profile of the new site administrator here %s.
+
+If you believe this was done in error, please login here %s and undo this action.",
+	'security_tools:notify_admins:remove_admin:subject' => "A site administrator was removed from %s",
+	'security_tools:notify_admins:remove_admin:message' => "Hi,
+
+The user %s is no longer a site administrator. This action was taken by %s.
+Check out the profile of the old site administrator here %s.
+
+If you believe this was done in error, please login here %s and undo this action.",
+
 );
 
 add_translation("en", $english);
---
+++ lib/events.php
@@ -0,0 +1,107 @@
+<?php
+/**
+ * All event handler callback functions are handled in this file
+ */
+
+/**
+ * Event is called when an user is made admin on the site
+ *
+ * @param string   $event make_admin
+ * @param string   $type  user
+ * @param ElggUser $user  the affected user
+ *
+ * @return void
+ */
+function security_tools_make_admin_handler($event, $type, ElggUser $user) {
+	
+	if (!empty($user) && elgg_instanceof($user, "user")) {
+		// notify other administrators about this
+		if (elgg_get_plugin_setting("mails_admin_admins", "security_tools") != "no") {
+			$site = elgg_get_site_entity();
+			$logged_in_user = elgg_get_logged_in_user_entity();
+			
+			// get all the site administrators
+			$options = array(
+				"limit" => false,
+				"joins" => array("JOIN " . elgg_get_config("dbprefix") . "entity_relationships r ON e.guid = r.guid_one"),
+				"wheres" => array("(r.relationship = 'member_of_site' AND r.guid_two = " . $site->getGUID() . ")")
+			);
+			$admins = elgg_get_admins($options);
+			
+			// allow other plugins to modify the admins
+			$params = array(
+				"event" => "make_admin",
+				"admins" => $admins
+			);
+			$admins = elgg_trigger_plugin_hook("notify_admins", "security_tools", $params, $admins);
+			// if we have administrators left, notify them
+			if (!empty($admins) && is_array($admins)) {
+				$subject = elgg_echo("security_tools:notify_admins:make_admin:subject", array($site->name));
+				$message = elgg_echo("security_tools:notify_admins:make_admin:message", array(
+					$user->name,
+					$logged_in_user->name,
+					$user->getURL(),
+					$site->url
+				));
+				
+				foreach ($admins as $admin) {
+					// force notifications to email so nobody misses this
+					notify_user($admin->getGUID(), $site->getGUID(), $subject, $message, null, "email");
+				}
+			}
+		}
+	}
+}
+
+/**
+ * Event is called when the admin rights of an user are removed
+ *
+ * @param string   $event remove_admin
+ * @param string   $type  user
+ * @param ElggUser $user  the affected user
+ *
+ * @return void
+ */
+function security_tools_remove_admin_handler($event, $type, ElggUser $user) {
+	
+	if (!empty($user) && elgg_instanceof($user, "user")) {
+		// notify other administrators about this
+		if (elgg_get_plugin_setting("mails_admin_admins", "security_tools") != "no") {
+			$site = elgg_get_site_entity();
+			$logged_in_user = elgg_get_logged_in_user_entity();
+			
+			// get all the site administrators
+			$options = array(
+				"limit" => false,
+				"joins" => array("JOIN " . elgg_get_config("dbprefix") . "entity_relationships r ON e.guid = r.guid_one"),
+				"wheres" => array(
+					"(r.relationship = 'member_of_site' AND r.guid_two = " . $site->getGUID() . ")",
+					"(e.guid <> " . $user->getGUID() . ")"
+				)
+			);
+			$admins = elgg_get_admins($options);
+			
+			// allow other plugins to modify the admins
+			$params = array(
+				"event" => "remove_admin",
+				"admins" => $admins
+			);
+			$admins = elgg_trigger_plugin_hook("notify_admins", "security_tools", $params, $admins);
+			// if we have administrators left, notify them
+			if (!empty($admins) && is_array($admins)) {
+				$subject = elgg_echo("security_tools:notify_admins:remove_admin:subject", array($site->name));
+				$message = elgg_echo("security_tools:notify_admins:remove_admin:message", array(
+					$user->name,
+					$logged_in_user->name,
+					$user->getURL(),
+					$site->url
+				));
+				
+				foreach ($admins as $admin) {
+					// force notifications to email so nobody misses this
+					notify_user($admin->getGUID(), $site->getGUID(), $subject, $message, null, "email");
+				}
+			}
+		}
+	}
+}
