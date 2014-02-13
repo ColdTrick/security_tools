b6b75bdc7bd9963a9fc63dd7122c66caa9667539
Jerome Bakker <jeabakker@coldtrick.com>
2014-02-13 11:58:28 +0100

    cleaner display of warning

--- views/default/security_tools/site_secret.php
+++ views/default/security_tools/site_secret.php
@@ -4,12 +4,14 @@
  * to warn people about the change in upgrade.php code
  */
 
-$setting = elgg_get_plugin_setting("secure_upgrade", "security_tools");
-if ($setting != "no") {
+$plugin = elgg_get_plugin_from_id("security_tools");
+
+if (!empty($plugin) && ($plugin->secure_upgrade != "no")) {
+
 	$settings_url = "admin/plugin_settings/security_tools";
 	$settings_url = elgg_normalize_url($settings_url);
 	
-	echo "<div class='elgg-admin-notices mtm pbn'>";
-	echo "<p class='mbn'>" . elgg_echo("security_tools:site_secret:warning", array($settings_url)) . "</p>";
-	echo "</div>";
+	$content = elgg_view("output/longtext", array("value" => elgg_echo("security_tools:site_secret:warning", array($settings_url))));
+	
+	echo elgg_view_module("inline", $plugin->getFriendlyName(), $content);
 }
