95f01340094fa7c000436087059d4230dfd85896
Jerome Bakker <jeabakker@coldtrick.com>
2014-02-13 10:22:47 +0100

    code reset

--- languages/en.php
+++ languages/en.php
@@ -1,20 +1,15 @@
 <?php
+/**
+ * English translation for this plugin
+ */
 
-	$english = array(
-		'security_tools' => "Security Tools",
-		
-		// settings
-		'security_tools:settings:secure_upgrade' => "Secure upgrade.php",
-		'security_tools:settings:secure_upgrade:description' => "With a secured upgrade.php only logged in site administrators can run upgrade.php",
-		
-		'security_tools:settings:secure_cron' => "Secure CRON",
-		'security_tools:settings:secure_cron:description' => "A secured CRON can will only work if you add a code to the URL of the CRON. This code is: <b>%s</b> (for example: %s)",
-		'security_tools:settings:secure_cron:bypass' => "Allow administrators to bypass CRON security",
-		'security_tools:settings:secure_cron:bypass:description' => "When this is set to 'yes' an administrator can manualy trigger the CRON by going to the currect URL without a valida CRON code.",
-		
-		// secure cron
-		'security_tools:secure_cron:error:code' => "The provided CRON security code is invalid",
-		'' => "",
-	);
+$english = array(
+	'security_tools' => "Security Tools",
 	
-	add_translation("en", $english);
+	// settings
+	'security_tools:settings:secure_upgrade' => "Secure upgrade.php",
+	'security_tools:settings:secure_upgrade:description' => "With a secured upgrade.php only logged in site administrators can run upgrade.php",
+	
+);
+
+add_translation("en", $english);
--- start.php
+++ start.php
@@ -1,20 +1,25 @@
 <?php
+/**
+ * This file is included when the plugin starts
+ */
 
-	require_once(dirname(__FILE__) . "/lib/functions.php");
-	require_once(dirname(__FILE__) . "/lib/hooks.php");
-	
-	function security_tools_init(){
-		// check if we are running upgrade.php
-		if(defined("UPGRADING")){
-			// is upgrade.php secured
-			if(elgg_get_plugin_setting("secure_upgrade", "security_tools") != "no"){
-				admin_gatekeeper();
-			}
+require_once(dirname(__FILE__) . "/lib/functions.php");
+require_once(dirname(__FILE__) . "/lib/hooks.php");
+
+// register default Elgg events
+elgg_register_event_handler("init", "system", "security_tools_init");
+
+/**
+ * Function is called when the Elgg system initializes
+ *
+ * @return void
+ */
+function security_tools_init() {
+	// check if we are running upgrade.php
+	if (defined("UPGRADING")) {
+		// is upgrade.php secured
+		if (elgg_get_plugin_setting("secure_upgrade", "security_tools") != "no") {
+			admin_gatekeeper();
 		}
 	}
-	
-	// register default Elgg events
-	elgg_register_event_handler("init", "system", "security_tools_init");
-	
-	// register plugin hooks
-	elgg_register_plugin_hook_handler("route", "cron", "security_tools_cron_route_hook");
+}
